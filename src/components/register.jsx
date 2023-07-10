0a89ce880bf0b4cfe167bf69f17cc58c95f57517
tekhunt <chizzydmec@gmail.com>
2023-07-10 18:03:17 +0100

    Fix sign up bug

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -13,7 +13,7 @@ export const Register = () => {
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+              Sign up to your account
             </h2>
           </div>
   
